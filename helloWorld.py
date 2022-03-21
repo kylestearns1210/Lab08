697f763d4a3513539d24bfb12c8cf31ff0fe5b73
kylestearns1210 <102064544+kylestearns1210@users.noreply.github.com>
2022-03-21 15:34:21 -0400

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,11 @@
+print ("Hello World")
+print ("Hello World, my name is Kyle Stearns and I love Python!")
+userName = input ("Hello! Please enter you name!")
+print ("Hello", userName, "nice to meet you")
+
+
+userName = input("Hello! Please enter your facebook username! ")
+userPassword = input("Hello! Please enter your facebook password! ")
+if(userName.lower() == "kyle" and userPassword == "csf534"):print("Welcome to facebook!")
+
+else:print("Wrong credentials, try again!")
